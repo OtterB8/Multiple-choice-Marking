9fa8b94168cab89de330455b314cb84f19e6f1f4
baopdh <pdhbao@gmail.com>
2020-09-05 20:55:23 +0700

    update dependencies

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -14,7 +14,11 @@ cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local ..
 make -j4
 make install
 
+echo "Installing node..."
+apt-get update
+apt-get install nodejs
+
 echo "Installing yarn..."
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update && sudo apt install yarn
+apt update && sudo apt install yarn
